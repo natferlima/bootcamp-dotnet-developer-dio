5916186e578e213bdb72135a10dcb27850b5a875
natferlima <ferlima.natalia@gmail.com>
2022-09-16 22:12:40 -0300

    Propriedades, métodos e construtores

---
+++ ExemploExplorando/Program.cs
@@ -0,0 +1,16 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Natália";
+p1.Sobrenome = "Lima";
+p1.Idade = 20;
+p1.Apresentar();
+
+Pessoa p2 = new Pessoa("Ana", "Lima");
+
+Curso cursoDeInlges = new Curso();
+cursoDeInlges.Nome = "Inglês";
+cursoDeInlges.Alunos = new List<Pessoa>();
+cursoDeInlges.AdicionarAluno(p1);
+cursoDeInlges.AdicionarAluno(p2);
+cursoDeInlges.ListarAlunos();
