ba84dd2c014ea3cb0a372287a6034bdc4d358395
natferlima <ferlima.natalia@gmail.com>
2022-10-03 13:59:25 -0300

    Introdução, Abstração e Encapsulamento

---
+++ ExemplosPOO/Program.cs
@@ -0,0 +1,16 @@
+﻿using ExemplosPOO.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Natália";
+p1.Idade = 25;
+
+p1.Apresentar();
+
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+// c1.saldo = 5 não é possivel acessar pois é privado
+
+c1.ExibirSaldo();
+c1.Sacar(5000);
+c1.ExibirSaldo();
