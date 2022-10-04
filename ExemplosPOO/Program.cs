460715bce3566f36802fb6eb04dabba107aab8f6
natferlima <ferlima.natalia@gmail.com>
2022-10-04 15:50:49 -0300

    Herança e Polimorfismo

--- ExemplosPOO/Program.cs
+++ ExemplosPOO/Program.cs
@@ -14,3 +14,17 @@ ContaCorrente c1 = new ContaCorrente(123, 1000);
 c1.ExibirSaldo();
 c1.Sacar(5000);
 c1.ExibirSaldo();
+
+
+Aluno a1 = new Aluno();
+a1.Nome = "Natália";
+a1.Idade = 25;
+a1.Nota = 10;
+a1.Apresentar();
+
+
+Professor prof1 = new Professor();
+prof1.Nome = "Ana";
+prof1.Idade = 25;
+prof1.Salario = 1000;
+prof1.Apresentar();
